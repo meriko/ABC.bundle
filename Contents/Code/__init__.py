1198e3cc38979336c875c6d2248eca2ec93f8905
Sander <sander@plex.tv>
2014-12-26 14:49:54 +0100

    Fix for inconsistency in watchabc.go.com/abc.go.com base urls.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,8 @@
 NAME = "ABC"
 BASE_URL = "http://abc.go.com"
-SHOWS = "http://cdn.abc.go.com/vp2/ws-supt/s/syndication/2000/rss/001/001/-1/-1/-1/-1/-1/-1"
-SEASONS = "http://abc.go.com/vp2/s/carousel?service=seasons&parser=VP2_Data_Parser_Seasons&showid=%s&view=season"
-EPISODES = "http://abc.go.com/vp2/s/carousel?service=playlists&parser=VP2_Data_Parser_Playlist&postprocess=VP2_Data_Carousel_ProcessPlaylist&showid=%s&seasonid=%s&vidtype=lf&view=showplaylist&playlistid=PL5515994&start=0&size=100&paging=1"
+SHOWS = "http://watchabc.go.com/vp2/ws-supt/s/syndication/2000/rss/001/001/-1/-1/-1/-1/-1/-1"
+SEASONS = "http://watchabc.go.com/vp2/s/carousel?service=seasons&parser=VP2_Data_Parser_Seasons&showid=%s&view=season"
+EPISODES = "http://watchabc.go.com/vp2/s/carousel?service=playlists&parser=VP2_Data_Parser_Playlist&postprocess=VP2_Data_Carousel_ProcessPlaylist&showid=%s&seasonid=%s&vidtype=lf&view=showplaylist&playlistid=PL5515994&start=0&size=100&paging=1"
 
 RE_SHOW_ID = Regex('/(SH\d+)')
 RE_AIR_DATE = Regex('(?P<month>\d{2})/(?P<day>\d{2})/(?P<year>\d{2})$')
@@ -86,12 +86,18 @@ def Episodes(title, show_id, season):
 	html = GetHTML(EPISODES % (show_id, season))
 
 	for episode in html.xpath('//div[contains(@class, "reg_tile")]'):
+
 		url = episode.xpath('./div[@class="tile_title"]/a/@href')[0]
-		if not url.startswith('http://'):
-			url = BASE_URL + url
 
 		if not '/VDKA' in url and not '/redirect/fep?videoid=VDKA' in url:
 			continue
+
+		if not url.startswith('http://'):
+
+			if '/redirect/' in url:
+				url = 'http://abc.go.com%s' % (url)
+			else:
+				url = 'http://watchabc.go.com%s' % (url)
 
 		if '/watch/this-week/' in url:
 			url = url.replace('abc.go.com', 'abcnews.go.com')
